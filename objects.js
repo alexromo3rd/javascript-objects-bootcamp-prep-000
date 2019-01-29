95ee90ca0e71b1ca7c417ec41631dec356970d9c
Alex Romo <ayromo08@gmail.com>
2019-01-29 04:05:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
   'Linkin Park': 'Numb',
   'Eminem': 'Rap God'
 };
+
+function updatePlaylist(obj, artistName, songTitle) {
+  return Object.assign(obj, { artistName: songTitle} );
+}
